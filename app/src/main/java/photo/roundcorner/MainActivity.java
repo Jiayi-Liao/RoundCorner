11916dc138dea433ea8d9925fadf919a4467af85
buptljy <bupt_ljy@163.com>
2015-10-21 16:14:47 +0800

    first commit

---
+++ app/src/main/java/photo/roundcorner/MainActivity.java
@@ -0,0 +1,106 @@
+package photo.roundcorner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ImageView imageView;
+
+    private static final int IMAGE_PICK_REQUEST = 1;
+    private static final int CROP_REQUEST = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=(ImageView)findViewById(R.id.image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,null);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+                startActivityForResult(intent,IMAGE_PICK_REQUEST);
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode== Activity.RESULT_OK){
+            if(requestCode==IMAGE_PICK_REQUEST){
+                Uri uri=data.getData();
+                startImageCrop(uri,200,200,CROP_REQUEST);
+            }else if(requestCode==CROP_REQUEST){
+                Bundle extras=data.getExtras();
+                if(extras!=null){
+                    Bitmap bitmap=extras.getParcelable("data");
+                    if(bitmap!=null){
+                        bitmap=toRoundCorner(bitmap);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                }
+            }
+        }
+    }
+
+    public Bitmap toRoundCorner(Bitmap bitmap){
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 30;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
+
+    public void startImageCrop(Uri uri, int outputX, int outputY,
+                              int requestCode) {
+        Intent intent = null;
+        intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", outputX);
+        intent.putExtra("outputY", outputY);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", true);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", false); // face detection
+        startActivityForResult(intent, requestCode);
+    }
+
+}
